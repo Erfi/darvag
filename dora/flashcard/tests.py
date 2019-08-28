1c2f1589129b5b7c78bdcb260435893be639cac8
Erfan Azad <erfan.azad@tiliter.com>
2019-08-28 17:28:17 +0200

    added more tests, for resolving urls and views (going back and forth)

--- dora/flashcard/tests.py
+++ dora/flashcard/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from flashcard.views import home
+from django.contrib.auth.models import User
+from flashcard.views import home, lang_entry
+from flashcard.models import Entry
 
 
 class HomeTests(TestCase):
@@ -12,3 +14,28 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+
+class EntryPageTests(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        Entry.objects.create(from_lang='english',
+                             to_lang='german',
+                             from_word='hot',
+                             to_word='heiß',
+                             from_example='Der Kuchen ist heiß',
+                             created_by=user
+                             )
+
+    def test_lang_entry_view_success_status_code(self):
+        url = reverse('lang_entry', kwargs={'from_lang':'english'})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+
+    def test_entry_url_resolves_lang_entry_view(self):
+        view = resolve('/entry/english/')
+        self.assertEquals(view.func, lang_entry)
+
+
